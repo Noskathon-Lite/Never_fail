84d9215f7c45d2f990f911d805001bba6aa7f6da
hamroSamaya <yaadmaatmro2gmail.com>
2025-01-13 13:35:26 +0545

    back

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from PIL import Image
 import pytesseract
-
 import io
 
 app = Flask(__name__)
